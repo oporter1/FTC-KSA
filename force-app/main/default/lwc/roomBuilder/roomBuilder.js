0a48e6b5100b8c80e4837b2b7e828b26b4517a74
Emil Sabri <emil.sabri@gmail.com>
2025-03-26 12:28:51 -0700

    getAthletesAndRooms()

--- force-app/main/default/lwc/roomBuilder/roomBuilder.js
+++ force-app/main/default/lwc/roomBuilder/roomBuilder.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
+import getAthletesAndRooms from '@salesforce/apex/roomBuilder.getAthletesAndRooms';
 
-export default class RoomBuilder extends LightningElement {}
+export default class RoomBuilder extends LightningElement {
+    @api
+    get webcode() {
+        return this._webcode || ''
+    }
+    set webcode(value) {
+        if (!value) return
+
+        this._webcode = value
+        console.log('webCode - ', value)
+        getAthletesAndRooms({ webcode: value }).then(resp => {
+            console.log('Athletes and Rooms data - ', resp);
+        })
+    }
+    athletesAndRooms;
+
+    connectedCallback() {
+        console.log('lwc room builder connected - ', this.webcode);
+    }
+
+    renderedCallback() {
+        console.log('lwc room builder rendered - ', this.webcode);
+    }
+}
